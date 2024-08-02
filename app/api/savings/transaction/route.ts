59bf06b7e736e8ff75e92cd42c5c639625429451
Daniel Tejeda <daviddan1998@gmail.com>
2024-08-02 18:28:58 -0400

    feat(savings): fix missing import, and misc logic

--- app/api/savings/transaction/route.ts
+++ app/api/savings/transaction/route.ts
@@ -2,6 +2,7 @@ import { authOptions } from "@/config/authOptions";
 import { createSavingsTransaction } from "@/controllers/savingsController";
 import { getServerSession } from "next-auth/next";
 import { NextRequest, NextResponse } from "next/server";
+import mongoose from "mongoose";
 
 export async function POST (req: NextRequest) {
     const session = await getServerSession(authOptions)
@@ -9,13 +10,13 @@ export async function POST (req: NextRequest) {
         return NextResponse.json({ message: "Must be logged in"}, {status: 401})
     }
 
-    const userId = mongoose.Types.ObjectId(session.user.id);
+    const userId = new mongoose.Types.ObjectId(session.user.id);
 
     try {
         const request = await req.json();
         const validationErrors = validatePOSTFields(request);
         if (validationErrors.length > 0) {
-            return NextResponse.json({success: false, error: `${validationErrors.join(", ")} field${validationErrors.length > 1 ? 's' : null} missing from body request`}, {status: 400});
+            return NextResponse.json({success: false, error: `${validationErrors.join(", ")} field${validationErrors.length > 1 ? 's' : ''} missing from body request`}, {status: 400});
         }
         const transaction = await createSavingsTransaction(userId, request);
 
@@ -35,7 +36,7 @@ function validatePOSTFields (request: any): string[] {
     const missingFields = [];
 
     // Is the field missing?
-    for (let field in expectedFields) {
+    for (let field of expectedFields) {
         if (request[field] === undefined || request[field] === null) {
             missingFields.push(field);
         }
