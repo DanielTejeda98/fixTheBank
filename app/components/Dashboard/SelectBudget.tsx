771513f6d018087fb2d73832f6813bd3c48b207e
Daniel David Tejeda <daviddan1998@gmail.com>
2025-10-19 20:14:07 -0400

    feat(budget-selector): rework budget date selector

--- app/components/Dashboard/SelectBudget.tsx
+++ app/components/Dashboard/SelectBudget.tsx
@@ -1,17 +1,20 @@
-import { getBudget, toggleBudgetShareSettings } from "@/app/lib/budgetApi";
+import { getBudget } from "@/app/lib/budgetApi";
 import { setBudget } from "@/redux/features/budget-slice";
 import { useAppSelector } from "@/redux/store";
-import { FormEvent, useReducer, useRef, useState } from "react";
+import { FormEvent, useMemo, useReducer, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import SimpleReactValidator from "simple-react-validator";
-import ToggleSlider from "../ToggleSlider";
 import { useSession } from "next-auth/react";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
-import { Input } from "../ui/input";
 import { formatDateInput } from "@/app/lib/renderHelper";
 import { DrawerBody, DrawerFooter, DrawerHeader, DrawerTitle } from "../ui/drawer";
 import { useFTBDrawer } from "../ui/ftbDrawer";
+import { Select, SelectContent, SelectItem, SelectTrigger } from "../ui/select";
+
+const BASE_YEAR = 2020;
+const FUTURE_YEARS = 2;
+const RENDER_YEARS_COUNT = new Date().getFullYear() - BASE_YEAR + FUTURE_YEARS + 1; // +1 to include for the zero index
 
 export default function SelectBudget () {
     const { setOpen: setOpenDrawer } = useFTBDrawer();
@@ -20,12 +23,34 @@ export default function SelectBudget () {
     const forceUpdate = useReducer(x => x + 1, 0)[1];
     const reduxDispatch = useDispatch();
     const date = useAppSelector((state) => state.budgetReducer.value.minDate);
-    const isShared = useAppSelector((state) => state.budgetReducer.value.isShared);
-    const shareCode = useAppSelector((state) => state.budgetReducer.value.shareCode);
-    const isOwner = useAppSelector((state) => state.budgetReducer.value.isOwner);
 
     const [budgetDate, setBudgetDate] = useState(date)
-    const [isBudgetShared, setIsBudgetShared] = useState(isShared);
+
+    const [selectedMonth, setSelectedMonth] = useState(new Date(budgetDate).getMonth());
+    const [selectedYear, setSelectedYear] = useState(new Date(budgetDate).getFullYear());
+
+    const handleSetToday = () => {
+        setDate(new Date());
+    }
+
+    const handleSetLastMonth = () => {
+        const currentDate = new Date(selectedYear, selectedMonth, 2);
+        currentDate.setMonth(currentDate.getMonth() - 1);
+        setDate(currentDate);
+    }
+
+    const handleSetNextMonth = () => {
+        const currentDate = new Date(selectedYear, selectedMonth, 2);
+        currentDate.setMonth(currentDate.getMonth() + 1);
+        setDate(currentDate);
+    }
+
+    const setDate = (date: Date) => {
+        setSelectedMonth(date.getMonth());
+        setSelectedYear(date.getFullYear());
+        // We set the day index to 2 to avoid timezone issues that may cause date to shift back a day
+        setBudgetDate(formatDateInput(new Date(date.getFullYear(), date.getMonth(), 2)));
+    }
 
     const submitBudgetUpdates = async (form: FormEvent) => {
         form.preventDefault();
@@ -37,8 +62,6 @@ export default function SelectBudget () {
         }
 
         try {
-            await processBudgetShareUpdates({userId});
-
             const res = await getBudget({userId}, budgetDate)
             if (res.success) {
                 sessionStorage.setItem("selectedBudgetDate", budgetDate);
@@ -54,36 +77,14 @@ export default function SelectBudget () {
         setOpenDrawer(false);
     }
 
-    const processBudgetShareUpdates = async (headers: any) => {
-        if (isShared === isBudgetShared) {
-            return;
-        }
-
-        try {
-            const res = await toggleBudgetShareSettings(headers);
-            if (!res.success) {
-                console.log(res.error)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const renderShareOptions = () => {
-        if (!isOwner) {
-            return null
-        }
-        return (
-            <div className="mt-3">
-            <ToggleSlider name="sharedBudget" 
-                          id="is-shared-budget"
-                          label="Share budget?"
-                          state={isBudgetShared} 
-                          onChange={(value:boolean) => setIsBudgetShared(value)}/>
-            {isBudgetShared ? (<p>Share this code with family and friends so they can join: {shareCode}</p>) : null}
-            </div>
-        )
-    }
+    const yearOptions = useMemo(() => {
+        return Array.from({length: RENDER_YEARS_COUNT}, (_, i) => { {
+            const year = BASE_YEAR + i;
+            return (<SelectItem key={year} value={year.toString()}>
+                <p>{year}</p>
+            </SelectItem>)
+        }})
+    }, []);
 
     return (
     <form className="flex flex-col w-full overflow-scroll" onSubmit={submitBudgetUpdates}>
@@ -91,15 +92,51 @@ export default function SelectBudget () {
             <DrawerTitle>Select the budget month</DrawerTitle>
         </DrawerHeader>
         <DrawerBody className="flex flex-col">
-            <div className="w-full">
-                <Label htmlFor="date">Budget Date</Label>
-                <div className="flex w-full gap-2">
-                    <Input type="date" name="date" value={budgetDate} onChange={e => setBudgetDate(e.target.value)}/>
-                    <Button type="button" onClick={e => setBudgetDate(formatDateInput(new Date()))}>Today</Button>
+            <div className="flex items-center w-full gap-2">
+                <div className="flex w-2/3 gap-2 flex-wrap">
+                    <Label htmlFor="month" className="w-full">Budget Month</Label>
+                    <Select name="month" value={selectedMonth.toString()} onValueChange={(value) => {
+                        setSelectedMonth(parseInt(value));
+                        // We set the day index to 2 to avoid timezone issues that may cause date to shift back a day
+                        setBudgetDate(formatDateInput(new Date(selectedYear, parseInt(value), 2)));
+                    }}>
+                        <SelectTrigger className="w-full" >
+                            <p>{new Date(0, selectedMonth).toLocaleString('en-US', { month: 'long' })}</p>
+                        </SelectTrigger>
+                        <SelectContent>
+                            {[...Array(12)].map((_, index) => {
+                                const monthValue = index.toString();
+                                return (<SelectItem key={monthValue} value={monthValue}>
+                                    <p>{new Date(0, index).toLocaleString('en-US', { month: 'long' })}</p>
+                                </SelectItem>
+                                )
+                            })}
+                        </SelectContent>
+                    </Select>
                 </div>
-                { validator.current.message("date", budgetDate, "required") }
+
+                <div className="flex w-1/3 gap-2 flex-wrap">
+                    <Label htmlFor="year" className="w-full">Budget Year</Label>
+                    <Select name="year" value={selectedYear.toString()} onValueChange={(value) => {
+                        setSelectedYear(parseInt(value));
+                        // We set the day index to 2 to avoid timezone issues that may cause date to shift back a day
+                        setBudgetDate(formatDateInput(new Date(parseInt(value), selectedMonth, 2)));
+                    }}>
+                        <SelectTrigger className="w-full" >
+                            <p>{selectedYear}</p>
+                        </SelectTrigger>
+                        <SelectContent>
+                            {yearOptions}
+                        </SelectContent>
+                    </Select>
+                </div>
+
             </div>
-            {renderShareOptions()}
+            <Button type="button" onClick={handleSetToday}>Today</Button>
+            <div className="flex w-full gap-2">
+                <Button type="button" onClick={handleSetLastMonth} className="w-full" variant={'outline'}>Last Month</Button>
+                <Button type="button" onClick={handleSetNextMonth} className="w-full" variant={'outline'}>Next Month</Button>
+            </div>
         </DrawerBody>
         
         <DrawerFooter className="w-full">
